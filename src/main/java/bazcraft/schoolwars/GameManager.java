2038e2f46f398a0cb17a894b9dcf26ef0e43550a
NoahUyttebroeck <noah.uyttebroeck@bazandpoort.be>
2021-11-03 13:01:34 +0100

    Teams
    De speler joined nu automatisch een team wanneer hij de server joined

--- src/main/java/bazcraft/schoolwars/GameManager.java
+++ src/main/java/bazcraft/schoolwars/GameManager.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GameManager {
 
@@ -21,13 +22,42 @@ public class GameManager {
 
     public boolean addSpeler(Player speler) {
         if (ingamePlayers.size() < maxAantalSpelers) {
+
             ingamePlayers.add(speler);
+
             if (ingamePlayers.size() == maxAantalSpelers) {
                 GameState.setGamestate(GameState.STARTING);
             }
+
+            sorteerSpelerInTeam(speler);
+
             return true;
+
         } else {
             return false;
+        }
+    }
+
+    private void sorteerSpelerInTeam(Player speler) {
+        Random rand = new Random();
+
+        Team blauw = plugin.getTeamManager().getBLUE();
+        Team rood = plugin.getTeamManager().getRED();
+
+        //Als er meer spelers in rood zijn dan blauw add speler in blauw
+        if (rood.getScoreboard().getEntries().size() > blauw.getScoreboard().getEntries().size()) {
+            blauw.addSpeler(speler);
+        } else if (rood.getScoreboard().getEntries().size() < blauw.getScoreboard().getEntries().size()){
+            //Als er meer spelers in blauw zijn dan rood add speler in rood
+            rood.addSpeler(speler);
+        } else {
+            //Als er evenveel spelers zijn add speler in random team
+            int random = rand.nextInt(2);
+            if (random > 0) {
+                blauw.addSpeler(speler);
+            } else {
+                rood.addSpeler(speler);
+            }
         }
     }
 
---
+++ src/main/java/bazcraft/schoolwars/Team.java
@@ -0,0 +1,49 @@
+package bazcraft.schoolwars;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class Team {
+
+    private final org.bukkit.scoreboard.Team scoreboard;
+    private int health;
+    private final Location spawn;
+
+    public Team(String naam, int health, Location spawn, ChatColor color) {
+
+        org.bukkit.scoreboard.Team temp = Bukkit.getScoreboardManager().getMainScoreboard().getTeam(naam);
+        if (temp == null) {
+            temp = Bukkit.getScoreboardManager().getMainScoreboard().registerNewTeam(naam);
+        }
+        scoreboard = temp;
+
+        this.health = health;
+        this.spawn = spawn;
+
+        scoreboard.setAllowFriendlyFire(false);
+        scoreboard.setColor(color);
+
+    }
+
+    public void addSpeler(Player speler) {
+        scoreboard.addEntry(speler.getName());
+    }
+
+    public org.bukkit.scoreboard.Team getScoreboard() {
+        return scoreboard;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public Location getSpawn() {
+        return spawn;
+    }
+}
