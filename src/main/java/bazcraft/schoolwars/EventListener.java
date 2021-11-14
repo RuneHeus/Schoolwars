36640e25b2b26a78ff4762404faa5e16a4df0ae3
RuneHeus <83946705+RuneHeus@users.noreply.github.com>
2021-11-14 23:34:30 +0100

    Update NPC Vragen GUI
    Deze heeft nog een kleine null pointer bug (niet server ruinerend).

--- src/main/java/bazcraft/schoolwars/EventListener.java
+++ src/main/java/bazcraft/schoolwars/EventListener.java
@@ -3,18 +3,25 @@ package bazcraft.schoolwars;
 import bazcraft.schoolwars.GUI.VragenGUI;
 import bazcraft.schoolwars.Vragen.Vraag;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
+import net.citizensnpcs.npc.ai.speech.Chat;
+import org.apache.commons.lang.ObjectUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
 
 public class EventListener implements Listener {
 
@@ -67,8 +74,15 @@ public class EventListener implements Listener {
 
     @EventHandler
     public void onGuiClick(InventoryClickEvent event){
+        ItemStack book = plugin.getVragenManager().getVragenLijst().get(0).getBook();
+        Player player = (Player) event.getWhoClicked();
+        Material clickedItem = Objects.requireNonNull(event.getCurrentItem()).getType();
         if(event.getView().getTitle().equalsIgnoreCase(ChatColor.AQUA + "Vragen Menu")){
-            System.out.println(ChatColor.GREEN + "TEST TEST TEST TEST!!!!!!");
+            if(clickedItem.equals(Material.BOOK)){
+                player.openBook(book);
+            }else if(clickedItem.equals(Material.WRITTEN_BOOK)){
+                player.sendMessage(ChatColor.RED + "Debug: " + ChatColor.GREEN + "Normaal moete nu een antwoord kunnen geven");
+            }
             event.setCancelled(true);
         }
     }
